191ca83776a53bf5fc5a2af3cae8346bbabea162
Abi Klein <abi920@gmail.com>
2017-08-15 10:10:52 -0400

    adds vuejs movie project

---
+++ src/components/Overview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="overview">
+    <day-select v-bind:selected="day"></day-select>
+    <div id="main">
+      <movie-list v-bind:genre="genre" v-bind:time="time" v-bind:movies="movies" v-bind:day="day"></movie-list>
+      <movie-filter></movie-filter>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import DaySelect from './Dayselect.vue';
+import MovieList from './MovieList.vue';
+
+import MovieFilter from './MovieFilter.vue';
+  export default {
+    props: [ 'genre', 'time', 'movies', 'day'],
+    components: {
+      MovieList,
+      MovieFilter,
+      DaySelect
+    },
+  };
+
+</script>
